4982dee0c2c7626fc14029ab9dde0f24b666ddd9
Shubham Kadam <shubhamskadam89@gmail.com>
2026-02-18 23:27:35 +0530

    Replace `application.properties` with `application.yml`, configure logging using Logback, and refine project setup:
    - Migrated configurations to `application.yml` with improved structure and additional settings for JPA, logging, and management.
    - Added `logback-spring.xml` for detailed logging with various appenders (console, file, error-specific, and payment-specific logs).
    - Removed Lombok dependency from `pom.xml`.
    - Updated `PaymentPlatformApplication` to print a startup message.

--- src/main/java/com/spring/boot/levelup/payment/PaymentPlatformApplication.java
+++ src/main/java/com/spring/boot/levelup/payment/PaymentPlatformApplication.java
@@ -8,6 +8,7 @@ public class PaymentPlatformApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentPlatformApplication.class, args);
+        System.out.println("Welcome to payment service");
     }
 
 }
